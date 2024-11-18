a85e4e484cbe1312e293c9c9c85de463bfc6cf68
anjagnz <anja.n.gonzales@gmail.com>
2024-11-18 15:05:36 +0800

    transactions and reports sql scripts

---
+++ report-script.sql
@@ -0,0 +1,25 @@
+-- Report Script
+
+-- Concert Ticket Sales Report (total number of tickets sold per type) 
+-- per day, for a given year and month before the concert takes place (TBD by Anja)
+
+-- Concert Merchandise Sales Report (total number of merchandise sold per unique item)
+-- for the given day, month, and year the concert takes place (TBD by Anja)
+
+-- Concert Total Value of Sales Report (total value of tickets sold and total value of the
+-- merchandise sold (with consideration to the merchandise cut)) for a given year and month (TBD by Jat)
+
+-- Venue Rental Sales Report (total number of times rented for a concert) 
+-- per day, for a given year and month (TBD by DJ)
+
+-- Venue Merchandise Sales Report (total number of merchandise sold within the venue) 
+-- per day, for a given year and month (TBD by DJ)
+
+-- Venue Total Value of Sales Report (total value of rentals availed and the total value of the
+-- merchandise cuts from the total merchandise sold) per day, for a given year and month (TBD by Jat)
+
+-- User Purchase History Report (details of tickets bought, details 
+-- of merchandise bought) up to the current day (TBD by Anja)
+
+-- Employee Earnings Report (total earnings amassed of an employee that can be seen by themselves, or by an employee 
+-- with the Database Administrator role) from their date of hire to the current day (TBD by Jat)
---
+++ transaction-script.sql
@@ -0,0 +1,9 @@
+-- Transaction Script
+
+-- Scheduling a Concert as a Transaction (TBD by Anja)
+
+-- Buying a Ticket for a Concert as a Transaction (TBD by Jat)
+
+-- Buying Merchandise during a Concert as a Transaction (TBD by DJ)
+
+-- Cancellation of Concert as a Transaction (TBD by Anja)
