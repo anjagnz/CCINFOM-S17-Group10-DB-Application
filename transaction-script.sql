08481e85e747ad5bde05b0356e560b9457b60fcd
anjagnz <anja.n.gonzales@gmail.com>
2024-11-24 20:57:54 +0800

    FINAL

--- transaction-script.sql
+++
@@ -1,9 +0,0 @@
--- Transaction Script
-
--- Scheduling a Concert as a Transaction (TBD by Anja)
-
--- Buying a Ticket for a Concert as a Transaction (TBD by Jat)
-
--- Buying Merchandise during a Concert as a Transaction (TBD by DJ)
-
--- Cancellation of Concert as a Transaction (TBD by Anja)
